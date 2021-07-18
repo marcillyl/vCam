5154af4719967afc0c48ae2ee89c8dcd823948ab
marcillyl <marcillyl@icloud.com>
2021-07-18 17:20:18 +0200

    add default files

---
+++ frontend/src/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <HelloWorld msg="" />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+@font-face {
+  font-family: "OpenSans";
+  src: url("./assets/Fonts/Open_Sans/OpenSans-Light.ttf") format("truetype");
+  font-weight: 300;
+}
+@font-face {
+  font-family: "OpenSans";
+  src: url("./assets/Fonts/Open_Sans/OpenSans-Regular.ttf") format("truetype");
+  font-weight: 400;
+}
+@font-face {
+  font-family: "OpenSans";
+  src: url("./assets/Fonts/Open_Sans/OpenSans-SemiBold.ttf") format("truetype");
+  font-weight: 600;
+}
+@font-face {
+  font-family: "OpenSans";
+  src: url("./assets/Fonts/Open_Sans/OpenSans-Bold.ttf") format("truetype");
+  font-weight: 700;
+}
+@font-face {
+  font-family: "OpenSans";
+  src: url("./assets/Fonts/Open_Sans/OpenSans-ExtraBold.ttf") format("truetype");
+  font-weight: 800;
+}
+#app {
+  font-family: "OpenSans", Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+*,
+*::before,
+*::after {
+  box-sizing: inherit;
+  margin: 0;
+}
+html {
+  box-sizing: border-box;
+  scroll-behavior: smooth;
+  min-width: 320px;
+}
+img {
+  object-fit: cover;
+}
+a {
+  cursor: pointer;
+  text-decoration: none;
+}
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+  cursor: default;
+}
+</style>
