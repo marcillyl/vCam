ef3bdb51384c28fba1db8c503ae36da1c20dd2b7
marcillyl <marcillyluc@icloud.com>
2021-10-06 18:18:42 +0200

    build(app): dotenv config

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
+const path = require("path");
+require("dotenv").config;
 const express = require("express");
 const mongoose = require("mongoose");
-const path = require("path");
 const helmet = require("helmet");
 const cameraRoutes = require("./routes/camera");
 
